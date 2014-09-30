3f7aed8c256dc3db397aa73c845224a9aaf0678a
Maxim Komar <mk@mtw.ru>
2014-09-30 23:17:02 +0400

    lua: tracking table accesses

---
+++ lua/Programming in Lua/13.4.4.lua
@@ -0,0 +1,30 @@
+-- Tracking table access
+
+-- original table
+t = {}
+
+-- keep a private access to original table
+local _t = t
+
+-- create proxy
+t = {}
+
+local mt = {
+    __index = function (t, k)
+        print("*access to element " .. tostring(k))
+        return _t[k]    -- access the original table
+    end,
+
+    __newindex = function (t, k, v)
+        print("*update of element " .. tostring(k) .. " to " .. tostring(v))
+        _t[k] = v       -- update original table
+    end
+}
+setmetatable(t, mt)
+
+t[2] = 'hello'
+-- *update of element 2 to hello
+
+print(t[2])
+-- *access to element 2
+-- hello
