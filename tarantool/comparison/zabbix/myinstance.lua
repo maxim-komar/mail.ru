56bd0a0d4d80d567c4eecd35943ea9f9c414c5c8
Maxim Komar <mk@mtw.ru>
2014-09-30 20:03:37 +0400

    select data from tarantool

--- tarantool/comparison/zabbix/myinstance.lua
+++ tarantool/comparison/zabbix/myinstance.lua
@@ -64,3 +64,36 @@ select_from_trends_uint = function (itemid, from, to)
     end
     return #res
 end
+
+select_from_trends_uint2 = function (itemid, from, to)
+    ps = clock_to_part(from)
+    pf = clock_to_part(to)
+
+    ts = part_to_tname(ps)
+    tf = part_to_tname(pf)
+
+    local res = {}
+    if ts == tf then
+        local data = box.space[ts]:select({itemid}, {iterator = 'EQ'})
+        for i = 1, #data do
+            if (data[i][2] >= from) and (data[i][2] <= to) then table.insert(res, data[i]) end
+        end
+    else
+        local data = box.space[ts]:select({itemid}, {iterator = 'EQ'})
+        for i = 1, #data do
+            if (data[i][2] >= from) then table.insert(res, data[i]) end
+        end
+
+        for i = clock_to_part(from) + 1, clock_to_part(to) - 1 do
+            local tname = part_to_tname(i)
+            local data = box.space[tname]:select({itemid}, {iterator = 'EQ'})
+            for i = 1, #data do table.insert(res, data[i]) end
+        end
+
+        local data = box.space[tf]:select({itemid}, {iterator = 'EQ'})
+        for i = 1, #data do
+            if (data[i][2] <= to) then table.insert(res, data[i]) end
+        end
+    end
+    return res
+end
---
+++ tarantool/comparison/zabbix/tnt2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import sys
+import time
+from tarantool import Connection
+c = Connection("127.0.0.1", 3301)
+
+def conv(line):
+    return [ int(x) for x in line.split() ]
+
+filename = sys.argv[1]
+with open(filename) as f:
+    content = f.readlines()
+data = [ conv(line) for line in content ]
+
+if len(data) == 0:
+    sys.exit(0)
+
+def check_res(x):
+    res = c.call('select_from_trends_uint2', [x[1], x[2], x[3]])
+    rval = len(list(res))
+    return rval == x[0]
+
+start = time.time()
+for x in data:
+    if not check_res(x):
+        raise Exception(x)
+
+finish = time.time()
+
+print "%s\t%d\t%.2f\t%.6f" % (filename, len(data), finish - start, (finish - start) / len(data))
